a93ee9487dc98322efec4f1c219196dc8da80502
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-08-02 16:32:08 -0300

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/ar/edu/unlar/FinalP3/FinalP3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.FinalP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalP3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalP3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlar/FinalP3/FinalP3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.FinalP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalP3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
