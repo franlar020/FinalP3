dda4875bb0013269a41efec48347f44f982bce66
Francisco Antonio Gonzalez <ismatrumpet1616@gmail.com>
2026-08-02 21:45:12 -0300

    Eliminar comentario sobre la extensión de SpringBootServletInitializer en la clase FinalP3Application

--- src/main/java/ar/edu/unlar/FinalP3/FinalP3Application.java
+++ src/main/java/ar/edu/unlar/FinalP3/FinalP3Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication // @ComponentScan("ar.edu.unlar.FinalP3")
-public class FinalP3Application { // extends SpringBootServletInitializer
+public class FinalP3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalP3Application.class, args);
