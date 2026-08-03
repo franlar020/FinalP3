d24f3780d4fdca867f1e5e290dc46c29b18060bf
Francisco Antonio Gonzalez <ismatrumpet1616@gmail.com>
2026-08-02 21:35:34 -0300

    Agregar comentario en la anotación @SpringBootApplication para aclarar el escaneo de componentes

--- src/main/java/ar/edu/unlar/FinalP3/FinalP3Application.java
+++ src/main/java/ar/edu/unlar/FinalP3/FinalP3Application.java
@@ -3,7 +3,7 @@ package ar.edu.unlar.FinalP3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // @ComponentScan("ar.edu.unlar.FinalP3")
 public class FinalP3Application {
 
 	public static void main(String[] args) {
