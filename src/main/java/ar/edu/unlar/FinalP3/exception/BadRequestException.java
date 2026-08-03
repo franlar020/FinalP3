e1d371afd91923c50593c59360193edbf4c10c1b
Francisco Antonio Gonzalez <ismatrumpet1616@gmail.com>
2026-08-03 08:48:41 -0300

    Agregar clase BadRequestException para manejar excepciones de solicitudes incorrectas

---
+++ src/main/java/ar/edu/unlar/FinalP3/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package ar.edu.unlar.FinalP3.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
